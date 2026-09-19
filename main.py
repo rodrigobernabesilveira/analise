cf86954bfa3b6fa46754553aa726a39f75496eb3
rodrigo <rodrigosilveira.rbs@gmail.com>
2026-09-19 15:34:43 -0300

    a

--- main.py
+++ main.py
@@ -34,9 +34,7 @@ url = f"{base_url}?{encoded_params}"
 print(url)
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'application/atom+xml, application/xml, text/xml, */*',
-    'Accept-Language': 'en-US,en;q=0.9'
+    'User-Agent': 'User-Agent': 'MeuScriptDePesquisaAcademica/1.0 (contato: seu_email@exemplo.com)'
 }
 
 req_obj = urllib.request.Request(url, headers=headers)
