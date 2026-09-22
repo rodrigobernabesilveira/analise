4b9d38f4bdb76f51ee902573f16f090f3da9337a
rodrigobernabesilveira <rodrigosilveira.rbs@gmail.com>
2026-09-22 08:24:23 -0300

    Update User-Agent header in main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ url = f"{base_url}?{encoded_params}"
 print(url)
 
 headers = {
-    'User-Agent':'MeuScriptDePesquisaAcademica/1.0 (contato: seu_email@exemplo.com)',
-	'Accept': 'application/atom+xml, application/xml, text/xml, */*'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+    'Accept': 'application/atom+xml, application/xml, */*'
 }
 
 req_obj = urllib.request.Request(url, headers=headers)
